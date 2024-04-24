648c25465f1398a5532e1a99fa9b1141e1ad9171
Albert <67117216+Albert8419@users.noreply.github.com>
2024-04-24 01:57:16 +0000

    1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://silver-chainsaw-55x4pgvxg7rfj5g-3000.app.github.dev/api/aqi/london');
+        const response = await axios.get('https://silver-chainsaw-55x4pgvxg7rfj5g-3000.app.github.dev');
         setData(response.data);
         setLoading(false);
       } catch (error) {
